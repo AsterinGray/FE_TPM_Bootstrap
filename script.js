58ed68db71e6e7c903e14026072b680113cceb1e
AsterinGray <raymondtiandinatan@gmail.com>
2019-12-20 09:52:53 +0700

    new design assets added

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ hem.addEventListener('click',function(){
 	tempat.style.height='120px';
 	tempat.style.overflow='scroll';
 });
+
+let faq = 
+        document.getElementsByClassName("faq-title");
+
+        for (let i = 0; i < faq.length; i++){
+            faq[i].onclick = function (){
+            let content = this.nextElementSibling; // membaca object dari button 
+            this.classList.toggle('is-open'); // untuk membaca faq yg aktiv
+
+                if(content.style.maxHeight){
+                    // menutup faq
+                    content.style.maxHeight = null;
+                } 
+                else{
+                    // membuka faq
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            }
+        }
