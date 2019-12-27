addea91e09efe47a8af9d6da4d6ec2b0aa60f067
AsterinGray <raymondtiandinatan@gmail.com>
2019-12-28 01:53:05 +0700

    fixing

--- script.js
+++ script.js
@@ -7,25 +7,22 @@ let logo = document.querySelector('.logo');
 hem.addEventListener('click',function(){
     tempat.classList.toggle('navsbar');
     logo.style.display='none';
-    tempat.style.maxHeight='100vh';
+    tempat.style.height='100vh';
 });
 
-let faq = 
-        document.getElementsByClassName("faq-title");
-
-        for (let i = 0; i < faq.length; i++){
-            faq[i].onclick = function (){
-            let content = this.nextElementSibling; // membaca object dari button 
-            this.classList.toggle('is-open'); // untuk membaca faq yg aktiv
-
-                if(content.style.maxHeight){
-                    // menutup faq
-                    content.style.maxHeight = null;
-                } 
-                else{
-                    // membuka faq
-                    content.style.maxHeight = content.scrollHeight + "px";
-                }
+let faq = document.querySelectorAll(".faq-title");
+    for (let i = 0; i < faq.length; i++){
+        faq[i].onclick = function (){
+        let content = this.nextElementSibling; // membaca object dari button 
+        this.classList.toggle('is-open'); // untuk membaca faq yg aktiv
+            if(content.style.maxHeight){
+                // menutup faq
+                content.style.maxHeight = null;
+            } 
+            else{
+                // membuka faq
+                content.style.maxHeight = content.scrollHeight + "px";
             }
         }
+    }
         
